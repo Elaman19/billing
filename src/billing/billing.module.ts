e799ac084d3a4f31b8952a243ddc68fecfbac3b5
Elaman19 <30525069+Elaman19@users.noreply.github.com>
2024-01-18 15:26:58 +0600

    added docker files, configured env file, added new models, services, etc.

--- src/billing/billing.module.ts
+++ src/billing/billing.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BillingService } from './billing.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/user/models/user.model';
+import { Account, AccountSchema } from './model/account.model';
+import { Transaction, TransactionSchema } from 'src/transaction/model/transaction.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema },
+                               { name: Account.name, schema: AccountSchema },
+                               { name: Transaction.name, schema: TransactionSchema}]),
+  ],
   providers: [BillingService]
 })
 export class BillingModule {}
